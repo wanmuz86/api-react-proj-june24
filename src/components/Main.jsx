fd0e9d586fd4e45ec2d12497f209884cd9777d89
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 17:12:57 +0800

    update with error handling for non 200 cod

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,37 +4,45 @@ import Weather from './Weather'
 
 const Main = () => {
 
-const [weather, setWeather] = useState(null)
-const [loading, setLoading]  = useState(false)
+    const [weather, setWeather] = useState(null)
+    const [loading, setLoading] = useState(false)
 
-  {/* Lifting up state (4) Retrieve the city name from the method*/}
-const handleCitySearch = async (cityName) => {
+    {/* Lifting up state (4) Retrieve the city name from the method*/ }
+    const handleCitySearch = async (cityName) => {
 
-    setLoading(true)
-    console.log(cityName)
-
-    // Whenever there is word await on the function, async word needs to be added into function declaration
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9fd7a449d055dba26a982a3220f32aa2`);
-    const weatherData = await response.json();
-
-    setLoading(false)
-    setWeather(weatherData)
-
-}
-
-  return (
-    <div className='container py-3'>
-        {/*  Lifting up state (1) Add props citySearch to handle search Button pressed from child */}
-        <Search citySearch={handleCitySearch} />
-        {
-            weather !== null &&  <Weather weatherProp={weather}/>
-            
+        setLoading(true)
+        console.log(cityName)
+        try {
+            // Whenever there is word await on the function, async word needs to be added into function declaration
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=9fd7a449d055dba26a982a3220f32aa2`);
+            const weatherData = await response.json();
+    
+            setLoading(false)
+            if (weatherData.cod == 200){
+            setWeather(weatherData)
+            }
         }
-        {
-             loading &&    <p>Loading....</p>
+        catch (e) {
+            // handleError
+            console.log("error " +e)
+
         }
-    </div>
-  )
+
+    }
+    return (
+        <div className='container py-3'>
+            {/*  Lifting up state (1) Add props citySearch to handle search Button pressed from child */}
+            <Search citySearch={handleCitySearch} />
+
+            {
+                weather !== null && <Weather weatherProp={weather} />
+
+            }
+            {
+                loading && <p>Loading....</p>
+            }
+        </div>
+    )
 }
 
 export default Main
