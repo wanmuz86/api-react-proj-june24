8a600fe98344b9e76323feb746ee1fcab9e17918
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 15:57:20 +0800

    lift up city name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from './Search'
 import Weather from './Weather'
 
 const Main = () => {
+
+const [weather, setWeather] = useState(null)
+const handleCitySearch = (cityName) => {
+    console.log(cityName)
+
+}
+
   return (
     <div className='container py-3'>
-        <Search/>
+        <Search citySearch={handleCitySearch} />
         <Weather/>
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,23 @@
 import React,{useState} from 'react'
 
-const Search = () => {
-
+const Search = ({citySearch}) => {
+// 1) Create the state to store value of form
     const [cityForm, setCityForm] = useState('')
 
+    // 4) Update the state of the input into the state
     const handleCityForm = (e) => {
         setCityForm(e.target.value)
     }
 
     const searchPressed = () => {
-        console.log(cityForm);
+        citySearch(cityForm)
     }
 
     return (
         <div className="row g-3">
             <div className="col-8">
+                {/* 2. Add  value attribute on the input to link with the state*/}
+                 {/* 3. Add onChange method on input to handle value change on the form */}
                 <input type="text" className="form-control" id="cityName" 
                 placeholder="Enter City" value={cityForm} onChange={handleCityForm}/>
             </div>
