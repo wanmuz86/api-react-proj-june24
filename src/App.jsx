2dd769bd7564cd8ef36d28c1a9616bd19ddd4374
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 14:55:30 +0800

    Add bootstrap into project

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import  Header from './components/Header'
+import Header from './components/Header'
 import Footer from './components/Footer'
+import Main from './components/Main'
 
 function App() {
 
-
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
