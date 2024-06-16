b2fb42c21b9c796795c07480e44789b2a0a131d5
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 14:44:28 +0800

    Create different components and add into structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from './Search'
+import Weather from './Weather'
+
+const Main = () => {
+  return (
+    <div>
+        <Search/>
+        <Weather/>
+    </div>
+  )
+}
+
+export default Main
