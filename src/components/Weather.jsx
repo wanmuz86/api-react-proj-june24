55afa5111d4be72bb250f4ee0bc2dbbc337edca9
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 16:40:42 +0800

    add item

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 const Weather = ({weatherProp}) => {
+
+    const transformDt = (dt) => {
+        let date = new Date(dt * 1000)
+        return date.toLocaleString()
+    }
+
   return (
     <div className='card text-center p-3 bg-light'>
         <h2>{weatherProp?.name}</h2>
-        <img src="https://openweathermap.org/img/wn/04d@2x.png" alt=""  
+        <img src={`https://openweathermap.org/img/wn/${weatherProp?.weather[0].icon}@2x.png`} alt=""  
         width={200} 
         className='mx-auto'/>
-        <p>Weather , Subdescription</p>
+        <p>{weatherProp?.weather[0].main} , {weatherProp?.weather[0].description}</p>
         <p>{(weatherProp?.main.temp - 273.15).toFixed(2)} &deg;C</p>
-        <p>Humidity / Pressure</p>
-        <p>Sunset / Sunrise</p>
+        <p>{weatherProp?.main.humidity}% / {weatherProp?.main.humidity} hPa</p>
+        <p>Sunrise: {transformDt(weatherProp?.sys.sunrise)}</p>
+        <p>Sunset: {transformDt(weatherProp?.sys.sunset)}</p>
     </div>
   )
 }
