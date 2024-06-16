ca26cefbea607667ead20ad213ee6307d43d72b6
Wan Muzaffar <wanmuz86@gmail.com>
2024-06-16 15:45:14 +0800

    we retrieve city entered by user through the form + minor bug fixed on class

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Search = () => {
+
+    const [cityForm, setCityForm] = useState('')
+
+    const handleCityForm = (e) => {
+        setCityForm(e.target.value)
+    }
+
+    const searchPressed = () => {
+        console.log(cityForm);
+    }
+
     return (
-        <div class="row g-3">
-            <div class="col-8">
-                <input type="text" class="form-control" id="cityName" 
-                placeholder="Enter City" />
+        <div className="row g-3">
+            <div className="col-8">
+                <input type="text" className="form-control" id="cityName" 
+                placeholder="Enter City" value={cityForm} onChange={handleCityForm}/>
             </div>
-            <div class="col-4">
-                <button type="submit" class="btn btn-success 
-                mb-3" style={{width:'100%'}}>Search Weather</button>
+            <div className="col-4">
+                <button type="submit" className="btn btn-success 
+                mb-3" style={{width:'100%'}} onClick={searchPressed}>Search Weather</button>
             </div>
         </div>
     )
